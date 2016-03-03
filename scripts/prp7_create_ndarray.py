a689a07088b2b89f37e9a0436180f4cfe21ec5d3
Nikolaos Perrakis <np4g15@soton.ac.uk>
2016-03-03 18:21:58 +0000

    created distance matrix!

--- scripts/prp7_create_ndarray.py
+++ scripts/prp7_create_ndarray.py
@@ -21,7 +21,8 @@ print("Creted a numpy array of {} dimensions with {} elements!".format(dists.sha
 
 # 
 
-
+# Function that calculates document distance
+# takes sets of unique document words as inputs
 def cdist(a,b):
 	c = a & b
 	#print("Distance calculated at {}".format(d))
@@ -31,28 +32,32 @@ def cdist(a,b):
 
 # read word sets
 bsetlist = []
-with open("BoWsList.pickle", 'rb') as f:
+with open("../dataset/BoWsList.pickle", 'rb') as f:
     bsetlist = pickle.load(f)
 #print("Read Word Sets Successfully!")
 
-with open ("BoWsListNames.pickle", 'rb') as f:
+# read document names
+with open ("../dataset/BoWsListNames.pickle", 'rb') as f:
 	names = pickle.load(f)
+
+
 
 print("Let's see how many unique words we have in each document:")
 
 for key1,value1 in enumerate(bsetlist):
 	print("In document {} we have {} words!".format(names[key1],len(value1)))
 	
+# Now let's populate our document distance matrix
+for key1,value1 in enumerate(bsetlist):
+	for key2,value2 in enumerate(bsetlist):
+		if len(value1) != len(value2):
+			dists.itemset((key1,key2), cdist(value1, value2))
+		
 
+with open("../dataset/dists.pickle", 'wb') as f:
+	pickle.dump(dists, f)
 
-# Read File names
-
-
-
-
-
-
-
+print("Distance Matrix Saved Successfully!")
 
 
 
